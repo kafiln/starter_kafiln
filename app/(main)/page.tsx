a427cd82b7b26c7a9e8a0c47a760a1b3620193d7
Kafil Nasdami <kafil.nasdami@innovx.ma>
2025-02-26 13:24:44 +0000

    remove useless page

--- app/(main)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
-      {/* <Button>Click me</Button> */}
-    </div>
-  );
-}
