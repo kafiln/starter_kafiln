344e357e6c5f3fb5f1a2d3e26e292152073dca45
Kafil Nasdami <kafil.nasdami@innovx.ma>
2025-03-07 14:27:52 +0000

    fix landing page text dark

--- components/templates/feature-list-item.tsx
+++ components/templates/feature-list-item.tsx
@@ -11,7 +11,7 @@ const FeatureListItem: React.FC<{ features: Feature[] }> = ({ features }) => {
       {features.map(({ title, explanation }) => (
         <li key={title} className="p-4 ">
           <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-          <p className="text-gray-600">{explanation}</p>
+          <p className="text-gray-500 dark:text-gray-400">{explanation}</p>
         </li>
       ))}
     </ul>
